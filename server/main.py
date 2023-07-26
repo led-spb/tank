294f33110229e35785dd5a858876aba795c3e5df
Aleksey Ponimash <Aleksey.Ponimash@rt.ru>
2023-07-26 13:48:36 +0300

    distance method

--- server/main.py
+++ server/main.py
@@ -1,41 +1,56 @@
 import logging
 import socketserver
-import time
 from core.Motor import PWM
+from core.Ultrasonic import ultrasonic
 
 
 class CommandHandler(socketserver.BaseRequestHandler):
 
     def handle(self) -> None:
         try:
-            while(True):
+            while True:
                 data = self.request.recv(1024).strip()
                 if len(data) == 0:
                     return
 
                 try:
                     command = bytes.decode(data, encoding="utf-8")
+                    cmd, params = self.parse_input_command(command)
 
-                    cmd, params = command.split(':', 1)
-                    params = params.strip().split(' ')
-                    logging.debug(f'{cmd} {params}')
+                    retval = self.execute_command(cmd, params)
+                    response = f'ok: {retval}'
+                except BaseException as err:
+                    logging.exception('Error while executing command')
+                    response = f'err: {err}'
 
-                    self.execute_command(cmd, params)
-                except RuntimeError:
-                     logging.exception('Error while executing command')
-                self.request.sendall('ok'.encode('utf-8'))
+                self.request.sendall(response.encode('utf-8'))
         finally:
             PWM.setMotorModel(0, 0)
 
+    def parse_input_command(self, data):
+        parts = data.split(':', 1)
+        if len(parts) < 2:
+            raise ValueError('Malformed command')
+
+        command = parts[0]
+        params = parts[1].strip().split(' ')
+
+        logging.debug(f'{command} params={params}')
+        return command, params
+
     def execute_command(self, command, params):
+        retval = None
         if command == 'motor':
             left = int(params[0])
             right = int(params[1])
             logging.debug(f'{left} {right}')
             PWM.setMotorModel(int(params[0]), int(params[1]))
+            retval = 0
+        elif command == 'distance':
+            retval = ultrasonic.get_distance()
         else:
-            logging.error(f'Unknown command: {command}')
-        return
+            raise RuntimeWarning(f'Unknown command: {command}')
+        return retval
 
 
 class Server(socketserver.TCPServer):
--- tank/client.py
+++ tank/client.py
@@ -10,7 +10,11 @@ class Tank:
     def command(self, cmd, *args):
         params = " ".join([str(x) for x in args])
         self.sock.sendall(f'{cmd}: {params}'.encode('utf-8'))
-        return self.sock.recv(1024).decode('utf-8')
+        response = self.sock.recv(1024).decode('utf-8').strip()
+        return 0
 
     def motor(self, left: int, right: int) -> None:
         self.command('motor', left, right)
+
+    def distance(self) -> float:
+        return self.command('distance')
